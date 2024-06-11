8a93e17e4037295b885a900c3d473ce433c1a418
Océane Taquin <oceanetaquin2426@outlook.com>
2024-06-11 15:44:19 +0200

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,44 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
 
-export default router
+    {
+      path: "/projets",
+      redirect: "/404",
+    },
+
+    {
+      path: "/contact",
+      redirect: "/404",
+    },
+
+    {
+      path: "/404",
+      name: "Error404",
+      component: () => import("../views/Error404.vue"),
+    },
+
+    {
+      path: "/:catchAll(.*)",
+      redirect: "/404",
+    },
+  ],
+
+  scrollBehavior(to, from, savedPosition) {
+    if (to.hash) {
+      return { el: to.hash, behavior: "smooth" };
+    } else {
+      return { x: 0, y: 0, behavior: "smooth" };
+    }
+  },
+});
+
+export default router;
