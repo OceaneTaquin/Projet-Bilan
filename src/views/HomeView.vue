865a8b93c75e0f1dca61124a111ce919a3a64fd2
Océane Taquin <oceanetaquin2426@outlook.com>
2024-06-11 16:10:08 +0200

    Home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import MainElement from "@/components/MainElement.vue";
+import MyContact from "@/components/MyContact.vue";
+import { ref } from "vue";
+
+const showModal = ref(false);
+
+const toggleModal = (value) => {
+  showModal.value = value;
+};
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div id="WelcomeItem">
+      <h1>Bienvenue sur mon Portfolio !</h1>
+      <p>
+        Bonjour à vous ! Je m'appelle Taquin Océane, je suis actuellement
+        apprentie développeuse Web et Web mobile !
+      </p>
+    </div>
+
+    <router-link to="#section1" class="nav-link"
+      >Aller à la section Projets</router-link
+    >
+    <router-link to="#section2" class="nav-link"
+      >Aller à la section Pour me contacter !</router-link
+    >
+
+    <div id="section1" class="main-element-container">
+      <MainElement :showModal="showModal" :toggleModal="toggleModal" />
+    </div>
+    <div id="section2">
+      <MyContact />
+    </div>
   </main>
 </template>
