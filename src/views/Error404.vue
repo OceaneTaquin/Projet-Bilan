b656ad3cafc123440e5a4eb5dcce7f7e3c82bb31
Océane Taquin <oceanetaquin2426@outlook.com>
2024-06-11 19:55:11 +0200

    error

---
+++ src/views/Error404.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="error-page">
+    <h1>404 - Page introuvable</h1>
+    <p>La page que vous cherchez n'existe pas.</p>
+    <div class="error-image"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.error-page {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: "Playfair Display", serif;
+}
+
+.error-image {
+  width: 100%;
+  height: 100px;
+  background: url("../assets/images/error\ 404.png") center center/cover
+    no-repeat;
+}
+</style>
